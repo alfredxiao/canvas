b40f981730f0586fa904fa78b11d425b5773fbcf
Alfred Xiao <alfred.xiao@qantashotels.com>
2019-08-05 21:04:54 +1000

    dynamically load canvas lib rather than hardcoding it

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject canvas "0.1.0"
+(defproject canvas "0.1.1"
   :description "Clojure plugin for leiningen that reports on test coverage."
   :url "https://github.com/alfredxiao/canvas"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
--- src/leiningen/canvas.clj
+++ src/leiningen/canvas.clj
@@ -3,10 +3,14 @@
             [leiningen.core.main :as main])
   (:import (clojure.lang ExceptionInfo)))
 
+(defn- canvas-plugin-dep [project]
+  (->> (:plugins project)
+       (filter #(= (first %) 'canvas/canvas))
+       (first)))
+
 (defn ^:pass-through-help canvas
   [project & args]
-  (let [project (update-in project [:dependencies]
-                           conj ['canvas "0.1.0"])
+  (let [project (update-in project [:dependencies] conj (canvas-plugin-dep project))
         test-selectors (:test-selectors project)
         opts           {:src-ns-path (vec (:source-paths project))
                         :test-ns-path (vec (:test-paths project))}]
